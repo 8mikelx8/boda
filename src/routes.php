945ed033063f612b8b6a6827c34d9f49177f4b81
Miquel Correa <correa.miquel@gmail.com>
2017-09-02 13:05:09 +0200

    prepare level routes

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,14 @@
 <?php
 // Routes
 
-$app->get('/nivell', function ($request, $response, $args) {
+$app->get('/nivell/{level}', function ($request, $response, $args) {
     // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
+    $settings = $this->get('settings');
+    if(file_exists($settings['renderer']['template_path']."/levels/level".$args['level'].".phtml")) {
+      return $this->renderer->render($response, "levels/level".$args['level'].".phtml", $args);
+    } else {
+      return $this->renderer->render($response, "bad.phtml", $args);
+    }
 });
 
 
