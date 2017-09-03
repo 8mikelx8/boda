babf4d2135926c0625b5030e0730c07d1d8f7d1b
Miquel Correa <correa.miquel@gmail.com>
2017-09-03 14:12:56 +0200

    pass get params to level templates

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,7 @@ $app->get('/nivell/{level}', function ($request, $response, $args) {
     // Render index view
     $settings = $this->get('settings');
     if(file_exists($settings['renderer']['template_path']."/levels/level".$args['level'].".phtml")) {
+      $args['get'] = $request->getQueryParams();
       return $this->renderer->render($response, "levels/level".$args['level'].".phtml", $args);
     } else {
       return $this->renderer->render($response, "bad.phtml", $args);
