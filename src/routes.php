6ccc96ad8d361fef3d4c5439255fe40bc900cb6b
Miquel Correa <correa.miquel@gmail.com>
2017-09-05 18:09:39 +0200

    allow any method to level routes

--- src/routes.php
+++ src/routes.php
@@ -5,11 +5,12 @@ $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/nivell/{level}', function ($request, $response, $args) {
+$app->any('/nivell/{level}', function ($request, $response, $args) {
     // Render index view
     $settings = $this->get('settings');
     if(file_exists($settings['renderer']['template_path']."/levels/level".$args['level'].".phtml")) {
       $args['get'] = $request->getQueryParams();
+      $args['post'] = $request->getParsedBody();
       return $this->renderer->render($response, "levels/level".$args['level'].".phtml", $args);
     } else {
       return $this->renderer->render($response, "bad.phtml", $args);
